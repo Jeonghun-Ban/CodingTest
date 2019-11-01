b3f022ac6c4714293c07fa91bc6d8e87378b004f
Jeonghun-Ban <bjh5230@gmail.com>
2019-11-01 19:06:52 +0900

    Add Incomplete-Player

---
+++ Programmers/Incomplete-Player.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> pm = new HashMap<>();
+        for (String element: participant){
+            pm.put(element, pm.getOrDefault(element, 0)+1);
+        }
+        for (String element: completion){
+            pm.put(element, pm.get(element) - 1);
+        }
+        for (String key: pm.keySet()){
+            if(pm.get(key) != 0){
+                answer += key;
+            }
+        }
+
+        return answer;
+    }
+}
