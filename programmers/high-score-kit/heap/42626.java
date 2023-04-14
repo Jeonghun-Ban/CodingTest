e97115832386a9b1ef60a5de835ed16c802f0d99
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-04-14 17:57:26 +0900

    feat(programmers): Update 42626.java

--- programmers/high-score-kit/heap/42626.java
+++ programmers/high-score-kit/heap/42626.java
@@ -1,16 +1,28 @@
-import java.util.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 class Solution {
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        for (int food: scoville) q.offer(food);
-        
         int count;
-        for (count=0; q.peek() < K; count++){
-            // return -1; If the scoville of all foods does not exceed K
-            if (count == scoville.length -1){ count = -1; break; }
-            q.offer(q.poll() + 2*q.poll());
+        
+        Queue<Integer> foods = new PriorityQueue<>();
+        for(int s: scoville) {
+            foods.offer(s);
         }
+        
+        for(count = 0; foods.peek() < K; count++) {
+            if (count == scoville.length - 1) {
+                return -1;
+            }
+            foods.offer(makeNewFood(foods));
+        }
+        
         return count;
+    }
+    
+    private int makeNewFood(Queue<Integer> foods) {
+        int firstFood = foods.poll();
+        int secondFood = foods.poll();
+        return firstFood + (secondFood * 2);
     }
 }
