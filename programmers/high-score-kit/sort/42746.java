21d0374876ef2004281dcd5c57b2998f66670b8b
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-05-05 01:15:17 +0900

    feat(programmers): Update 64062.java

--- programmers/high-score-kit/sort/42746.java
+++ programmers/high-score-kit/sort/42746.java
@@ -2,28 +2,20 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
-        String num = "";
-        String[] nums = new String[numbers.length];
-
-        for (int i = 0; i < numbers.length; i++){
-            num = Integer.toString(numbers[i]);
-            nums[i] = num;
-        }
+        String[] nums = Arrays.stream(numbers)
+            .mapToObj(i -> String.valueOf(i)).toArray(String[]::new);
 
         Arrays.sort(nums, new Comparator<String>() {
-        @Override
-        public int compare(String s1, String s2) {
-            return (s2+s1).compareTo(s1+s2);
+            @Override
+            public int compare(String s1, String s2) {
+                return (s2+s1).compareTo(s1+s2);
             }
-          });
-
-        if (nums[0].equals("0")) return "0";
-
-        for (int j = 0; j <nums.length; j++){
-            answer += nums[j];
+        });
+        
+        if (nums[0].equals("0")) { 
+            return "0";
         }
 
-        return answer;
+        return String.join("", nums);
     }
 }
