afaaa4c711c5c2adf8d085ac880a788df8285f19
Jeonghun-Ban <junghoon.ban@gmail.com>
2023-06-03 14:16:44 +0900

    refactor(programmers): Update 43162.java

--- programmers/high-score-kit/dfs-bfs/43162.java
+++ programmers/high-score-kit/dfs-bfs/43162.java
@@ -1,23 +1,22 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int channel = 0;
-        int[] checked = new int[n];
-        
+        boolean[] checked = new boolean[n];
+
         for(int i=0; i<n; i++) {
-            if(checked[i] == 0) {
+            if(!checked[i]) {
                 dfs(i, computers, checked);
                 channel++;
             }
         }
         return channel;
     }
-    
-    
-    private void dfs(int i, int[][] computers, int[] checked) {
-        checked[i] = 1;
-        
+
+    private void dfs(int i, int[][] computers, boolean[] checked) {
+        checked[i] = true;
+
         for (int j = 0; j < computers.length; j++) {
-            if(i!=j && computers[i][j] == 1 && checked[j] == 0) {
+            if(i!=j && computers[i][j] == 1 && !checked[j]) {
                 dfs(j, computers, checked);
             }
         }
